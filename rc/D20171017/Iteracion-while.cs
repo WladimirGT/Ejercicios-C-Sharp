8587b453d21a8456034b4a66a93c40880af47328
WladimirGT <32730779+WladimirGT@users.noreply.github.com>
2017-10-17 21:42:29 -0300

    Add Iteracion-while.cs

---
+++ rc/D20171017/Iteracion-while.cs
@@ -0,0 +1,36 @@
+﻿/*
+** La instrucción while ejecuta una instrucción o un bloque de instrucciones hasta que una expresión especificada se evalúa como false.**
+
+while (expression) 
+{
+    //codigo
+	//declaracion
+ }
+
+*/
+
+/* 
+using System;
+
+namespace Ejercicios_C_Sharp
+{
+    class WhileTest2
+    {
+        static void Main()
+        {
+            int n = 3;
+            while (n++ < 6)
+            {
+                Console.WriteLine("Current value of n is {0}", n);
+            }
+        }
+    }
+}
+/*
+Output:
+Current value of n is 2
+Current value of n is 3
+Current value of n is 4
+Current value of n is 5
+Current value of n is 6
+*/
