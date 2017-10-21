347cda544e9d38e97e9f4e795fe94c5be7efcd01
WladimirGT <32730779+WladimirGT@users.noreply.github.com>
2017-10-21 17:34:39 -0300

    Update Suma-de-matrices2.cs

---
+++ rc/D20171021/Suma-de-matrices2.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] matrizA;
+            int[,] matrizB;
+            int[,] matrizR;
+
+            matrizA = new int[10, 10];
+            matrizB = new int[10, 10];
+            matrizR = new int[10, 10];
+
+
+            Console.WriteLine("Numeros matrizA");
+
+            for (int columna = 0; columna < 3; columna++)
+            {
+                for (int fila = 0; fila < 3; fila++)
+                {
+                    Random random = new Random();
+                    matrizA[columna, fila] = random.Next(1, 10);
+                    Console.Write("posicion matrizA [" + columna + "," + fila + "]: " + matrizA[columna, fila] + "\n");
+
+                    matrizB[columna, fila] = random.Next(1, 10);
+                    Console.Write("posicion matrizB [" + columna + "," + fila + "]: " + matrizB[columna, fila] + "\n");
+
+                    matrizR[columna, fila] = matrizA[columna, fila] + matrizB[columna, fila];
+                    Console.Write("posicion matrizC [" + columna + "," + fila + "]: " + matrizR[columna, fila] + "\n");
+
+
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
