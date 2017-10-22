6d95893b5f97cfc1c09fd51e44b80ac810d55da6
WladimirGT <32730779+WladimirGT@users.noreply.github.com>
2017-10-22 19:45:18 -0300

    Add Matriz-Diagonal-Inversa.cs

---
+++ rc/D20171022/Matriz-Diagonal-Inversa.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hola
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int[,] matriz;
+
+            Console.Write("ingrese el tamaño de la matriz:");
+            int tam = int.Parse(Console.ReadLine());
+            matriz = new int[tam, tam];
+
+            for (int i = 0; i < tam; i++)
+            {
+                for (int j = 0; j < tam; j++)
+                {
+                    Random random = new Random();
+                    matriz[i, j] = random.Next(10, 31);
+                    Console.Write("posicion matriz [" + i + "," + j + "]: " + matriz[i, j] + "\n");
+
+                }
+            }
+            int K = tam - 1;
+            for (int i = 0; i < tam; i++, K--)
+            {
+                for (int j = 0; j < tam; j++)
+                {
+                    if (K == j && matriz[i, j] > 10 && matriz[i, j] < 30)
+                    {
+                        Console.Write("posicion matriz [" + i + "," + j + "]: " + matriz[i, j] + "\n");
+                    }
+                    
+
+
+
+                }
+                
+            }
+
+        }
+    }
+}
