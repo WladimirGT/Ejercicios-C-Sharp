a999c309286d7a3e180cdf4098ec3ac5871ab2b8
WladimirGT <32730779+WladimirGT@users.noreply.github.com>
2017-10-24 20:22:45 -0300

    Add Matriz-Mayor-Menor.cs

---
+++ rc/D20171024/Matriz-Mayor-Menor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace MayorMenorLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz;
+
+
+            Console.Write("ingrese el tamaño de la matriz:");
+            int tam = int.Parse(Console.ReadLine());
+            matriz = new int[tam, tam];
+
+            for (int i = 0; i < tam; i++)
+            {
+                for (int j = 0; j < tam; j++)
+                {
+                    Random random = new Random();
+                    matriz[i, j] = random.Next(1, 100);
+                    Console.Write("posicion matriz [" + i + "," + j + "]: " + matriz[i, j] + "\n");
+                }
+            }
+
+            int mayor = matriz[0, 0];
+            int menor = matriz[0, 0];
+            for (int i = 0; i < tam; i++)
+            {
+                for (int j = 0; j < tam; j++)
+                {
+                    if (matriz[i, j] > mayor)
+                    {
+                        mayor = matriz[i, j];
+                    }
+                    else if (matriz[i, j] < menor)
+                    {
+                        menor = matriz[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("el numero menor es: " + menor);
+            Console.WriteLine("el numero mayor es: " + mayor);
+        }
+    }
+}
