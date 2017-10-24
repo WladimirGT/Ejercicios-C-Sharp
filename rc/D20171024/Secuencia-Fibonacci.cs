a82cde9e872d5a161320fd5684dab86d5ba1d455
WladimirGT <32730779+WladimirGT@users.noreply.github.com>
2017-10-24 16:16:25 -0300

    Add Secuencia-Fibonacci.cs

---
+++ rc/D20171024/Secuencia-Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hola
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 1;
+            int tam;
+
+            Console.Write("ingrese hasta donde quiere la secuencia fibonacci: ");
+            tam = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < tam; i++)
+            {
+                a = b;
+                b = c;
+                c = a + b;
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
