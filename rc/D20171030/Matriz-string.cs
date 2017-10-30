560ee0108f4d22855354aa3ba8ca1b3ff86238f1
WladimirGT <32730779+WladimirGT@users.noreply.github.com>
2017-10-30 20:31:07 -0300

    Add Matriz-string.cs

---
+++ rc/D20171030/Matriz-string.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace MayorMenorLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int contador = 0;
+            string[,] matriz = new string[8, 8]  {
+             {"CASAS", "ARBOL", "MAR", "IMAN", "NIDO", "OSO", "CUBO", "RATON"},
+             {"CASAS", "ARBOL", "MAR", "IMAN", "NIDO", "OSO", "CUBO", "RATON"},
+             {"CASAS", "ARBOL", "MAR", "IMAN", "NIDO", "OSO", "CUBO", "RATON"},
+             {"CASAS", "ARBOL", "MAR", "IMAN", "NIDO", "OSO", "CUBO", "RATON"},
+             {"CASAS", "ARBOL", "MAR", "IMAN", "NIDO", "OSO", "CUBO", "RATON"},
+             {"CASAS", "ARBOL", "MAR", "IMAN", "NIDO", "OSO", "CUBO", "RATON"},
+             {"CASAS", "ARBOL", "MAR", "IMAN", "NIDO", "OSO", "CUBO", "RATON"},
+             {"CASAS", "ARBOL", "MAR", "IMAN", "NIDO", "OSO", "CUBO", "RATON"}};
+
+             string p1 = "OSO";
+
+            {
+                //de izquierda a derecha(2)
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (p1 == matriz[i, j])
+                        {                           
+                            contador++;
+                        }
+                    }
+                }
+                Console.WriteLine(contador);
+            }
+        }
+    }
+}
+
